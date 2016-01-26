b0d92e8d272614c0e0c8319b83ded6e130b07c37
Mark Andrew <mark.andrew@de.bosch.com>
2016-01-26 17:14:46 +0100

    working b4 connect

--- index.js
+++ index.js
@@ -1,61 +1,68 @@
 var express = require('express');
 
-
 var app = express();
 
 function getmongoenv() {
+    var mongo = 0;
     if(process.env.VCAP_SERVICES){
 	var env = JSON.parse(process.env.VCAP_SERVICES);
-	var mongo = env['MongoDB-Service'][0]['credentials'];
+	mongo = env['MongoDB-Service'][0]['credentials'];
     }
     else {
-	var mongo = {
+	mongo = {
 	    "hostname":"localhost",
 	    "port":27017,
 	    "username":"",
 	    "password":"", 
 	    "name":"",
-	    "db":"db"
+	    "db":"foo"
 	}
-    return mongo
-}}
+    return mongo;
+    }
+}
 
-var generate_mongo_url = function(obj){
-  obj.hostname = (obj.hostname || 'localhost');
-  obj.port = (obj.port || 27017);
-  obj.db = (obj.db || 'test');
+var generate_mongo_url = function(obj) {
+    if(process.env.VCAP_SERVICES){
+	return obj.uri
+    }
+    if(obj.username && obj.password){
+	return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }
+    else {
+	return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }
+}
 
-  if(obj.username && obj.password){
-    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
-  }
-  else{
-    return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
-  }
+function getURI() {
+     if(process.env.VCAP_SERVICES){   
+	var env = JSON.parse(process.env.VCAP_SERVICES);
+	return (env['MongoDB-Service'][0]['credentials']['uri']);
+    } else {
+	return "mongodb://hostname:27017/db"
+    }
+
 }
 
 var port = (process.env.PORT || 3000);
  
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 app.get('/env', function (req, res) {
     res.write('this is the environment\n');
-
     for (var key in process.env) {
      	res.write(key + " : " + process.env[key] + "\n" );
     }
     res.end();
 });
 
-app.get('/mon', function (req, res) {
-    var mongo = getmongoenv();
-    var mongourl = generate_mongo_url(mongo);
+app.get('/', function (req, res) {
+    var mongourl = getURI();
     res.write(mongourl+'\n');
-    res.write(JSON.stringify(mongo)+'\n\n');
-    res.write(JSON.stringify(process.env.VCAP_SERVICES)+'\n');
+    //res.write(JSON.stringify(mongo)+'\n\n');
+    //res.write(JSON.stringify(process.env.VCAP_SERVICES)+'\n');
     res.end();
 });
+
+
 
 var server = app.listen(port, function () {
   var host = server.address().address;
