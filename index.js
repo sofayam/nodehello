fcaf735185510ecc13171b5a9fd9ee89c9e84c61
Mark Andrew <mark.andrew@de.bosch.com>
2016-01-26 14:48:25 +0100

    added mongo

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 var express = require('express');
+
+
 var app = express();
-var port = process.env.PORT || 3000;
+
+if(process.env.VCAP_SERVICES){
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  var mongo = env['MongoDB-Service'][0]['credentials'];
+}
+else{
+  var mongo = {
+    "hostname":"localhost",
+    "port":27017,
+    "username":"",
+    "password":"", 
+    "name":"",
+    "db":"db"
+  }
+}
+
+var generate_mongo_url = function(obj){
+  obj.hostname = (obj.hostname || 'localhost');
+  obj.port = (obj.port || 27017);
+  obj.db = (obj.db || 'test');
+
+  if(obj.username && obj.password){
+    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+  else{
+    return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+}
+
+var mongourl = generate_mongo_url(mongo);
+
+var port = (process.env.VMC_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+
  
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -14,7 +49,14 @@ app.get('/env', function (req, res) {
     }
     res.end();
 });
- 
+
+app.get('/mon', function (req, res) {
+    res.write(mongourl+'\n');
+    res.write(JSON.stringify(mongo)+'\n\n');
+    res.write(JSON.stringify(process.env.VCAP_SERVICES)+'\n');
+    res.end();
+});
+
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
